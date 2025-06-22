7304b204bc525a92a4aaae66544453930f088bfb
Davi Monteiro <d8119514@gmail.com>
2025-06-22 16:53:10 -0300

    mobile first v2

--- src/instance/api.ts
+++ src/instance/api.ts
@@ -7,10 +7,23 @@ const api = axios.create({
 // Interceptor para enviar token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) {
+  if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
+
+// Interceptor para logout automático em 401
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("userEmail");
+      window.location.href = "/login"; // redireciona pra login
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
