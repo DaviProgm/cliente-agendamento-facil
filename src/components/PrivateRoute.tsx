cc5c84487fcf700f0ed4acd4e76d28512f54078f
Davi Monteiro <d8119514@gmail.com>
2025-06-22 16:43:39 -0300

    corrijindo erro mobile

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,8 +1,8 @@
+// src/components/PrivateRoute.tsx
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const token = localStorage.getItem("token");
-
   return token ? children : <Navigate to="/login" />;
 };
 
