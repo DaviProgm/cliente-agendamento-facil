9cc7e0dd31b69bec14528f8c24a7ab55fd350b9a
Davi Monteiro <d8119514@gmail.com>
2025-06-22 16:50:28 -0300

    mobile first

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,9 +1,26 @@
-// src/components/PrivateRoute.tsx
 import { Navigate } from "react-router-dom";
+
+function isTokenValid(token: string | null) {
+  if (!token) return false;
+
+  try {
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    const now = Date.now() / 1000; // tempo atual em segundos
+    return payload.exp > now;
+  } catch {
+    return false;
+  }
+}
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/login" />;
+
+  if (!isTokenValid(token)) {
+    localStorage.removeItem("token");
+    return <Navigate to="/login" />;
+  }
+
+  return children;
 };
 
 export default PrivateRoute;
