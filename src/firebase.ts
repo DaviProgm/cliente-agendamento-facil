dbb563f7b70c1bdc1992580292a5a0081168b3c0
Davi Monteiro <d8119514@gmail.com>
2025-08-04 18:36:27 -0300

    atualização no manifest.json

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCiVNjAm654VESAvglmZUR-ZHHb72LHSqQ",
   authDomain: "cloktrix.firebaseapp.com",
   projectId: "cloktrix",
-  storageBucket: "cloktrix.firebasestorage.app",
+  storageBucket: "cloktrix.appspot.com",
   messagingSenderId: "744344387840",
   appId: "1:744344387840:web:a4749ce18ad10490a073e5",
   measurementId: "G-SGW0TQKSYH",
@@ -25,17 +25,20 @@ export const requestForToken = async (): Promise<string | null> => {
     return null;
   }
 
+
   try {
     const registration = await navigator.serviceWorker.register("/firebase-messaging-sw.js");
     console.log("✅ Service worker registrado.");
 
     const readyRegistration = await navigator.serviceWorker.ready;
     console.log("✅ Service Worker pronto. Tentando recuperar token...");
+    console.log("📡 Chamando getToken...");
 
     const currentToken = await getToken(messaging, {
       vapidKey: "BHsKE-EA8ZWChk1oAoucj9tgVSiQMUMquz79XynADRaHX0dsn2zOSwgzIkHbPyKA30G5AQ6bQQHmYX0Qds2BOB4",
       serviceWorkerRegistration: readyRegistration,
     });
+    console.log("📦 Token FCM:", currentToken);
 
     if (currentToken) {
       console.log("✅ Token FCM obtido com sucesso:", currentToken);
