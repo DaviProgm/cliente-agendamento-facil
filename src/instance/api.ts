3e74dfb6a46b35297bea9534908a6fe7f8182182
Davi Monteiro <d8119514@gmail.com>
2025-10-19 19:24:07 -0300

    darkmode && ligth mode

--- src/instance/api.ts
+++ src/instance/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: "https://vps62220.publiccloud.com.br:4005",
+  baseURL: "https://vps62220.publiccloud.com.br",
 });
 
 api.interceptors.request.use((config) => {
